dc1542907aaf33e8a3860b6ee4fea3dec22f0664
Marif Manafov <manafov_m@bk.ru>
2021-06-02 22:14:14 +0600

    added main method

---
+++ src/main/java/counterparties/CounterpartiesApp.java
@@ -0,0 +1,11 @@
+package counterparties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterpartiesApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CounterpartiesApp.class, args);
+    }
+}

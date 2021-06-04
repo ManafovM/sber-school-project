2c9a41e65c8795b587fcbb9dcbef506e55d4ec22
Marif Manafov <manafov_m@bk.ru>
2021-06-04 13:17:14 +0600

    enabled properties encryption

--- src/main/java/counterparties/CounterpartiesApp.java
+++ src/main/java/counterparties/CounterpartiesApp.java
@@ -1,9 +1,11 @@
 package counterparties;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class CounterpartiesApp {
     public static void main(String[] args) {
         SpringApplication.run(CounterpartiesApp.class, args);

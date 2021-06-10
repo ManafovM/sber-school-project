062c82d301936a85b12e212c414c557a62cb1856
Marif Manafov <manafov_m@bk.ru>
2021-06-10 16:39:19 +0600

    reformatted code

--- src/main/reactapp/src/App.js
+++ src/main/reactapp/src/App.js
@@ -1,67 +1,68 @@
 import './css/App.css';
 import {Component} from "react";
+
 const client = require('./client');
 
-class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {counterparties: []};
-  }
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {counterparties: []};
+    }
 
-  componentDidMount() {
-    client({method: 'GET', path: '/api/counterparties'}).done(response => {
-      this.setState({counterparties: response.entity._embedded.counterparties});
-    });
-  }
+    componentDidMount() {
+        client({method: 'GET', path: '/api/counterparties'}).done(response => {
+            this.setState({counterparties: response.entity._embedded.counterparties});
+        });
+    }
 
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <div className="App-intro">
-              <h2>Контрагенты</h2>
-              <CounterpartyList counterparties={this.state.counterparties}/>
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div className="App-intro">
+                        <h2>Контрагенты</h2>
+                        <CounterpartyList counterparties={this.state.counterparties}/>
+                    </div>
+                </header>
             </div>
-          </header>
-        </div>
-    );
-  }
+        );
+    }
 }
 
-class CounterpartyList extends Component{
-  render() {
-    const counterparties = this.props.counterparties.map(counterparty =>
-        <Counterparty key={counterparty._links.self.href} counterparty={counterparty}/>
-    );
-    return (
-        <table>
-          <tbody>
-          <tr>
-            <th>Наименование</th>
-            <th>ИНН</th>
-            <th>КПП</th>
-            <th>Номер счета</th>
-            <th>БИК</th>
-          </tr>
-          {counterparties}
-          </tbody>
-        </table>
-    )
-  }
+class CounterpartyList extends Component {
+    render() {
+        const counterparties = this.props.counterparties.map(counterparty =>
+            <Counterparty key={counterparty._links.self.href} counterparty={counterparty}/>
+        );
+        return (
+            <table>
+                <tbody>
+                <tr>
+                    <th>Наименование</th>
+                    <th>ИНН</th>
+                    <th>КПП</th>
+                    <th>Номер счета</th>
+                    <th>БИК</th>
+                </tr>
+                {counterparties}
+                </tbody>
+            </table>
+        )
+    }
 }
 
-class Counterparty extends Component{
-  render() {
-    return (
-        <tr>
-          <td>{this.props.counterparty.name}</td>
-          <td>{this.props.counterparty.tin}</td>
-          <td>{this.props.counterparty.iec}</td>
-          <td>{this.props.counterparty.accountNumber}</td>
-          <td>{this.props.counterparty.bic}</td>
-        </tr>
-    )
-  }
+class Counterparty extends Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.counterparty.name}</td>
+                <td>{this.props.counterparty.tin}</td>
+                <td>{this.props.counterparty.iec}</td>
+                <td>{this.props.counterparty.accountNumber}</td>
+                <td>{this.props.counterparty.bic}</td>
+            </tr>
+        )
+    }
 }
 
 export default App;

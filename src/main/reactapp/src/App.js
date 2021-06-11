b06c243cbafbc11ce0ceb4efa2ef3c72e132efba
Marif Manafov <manafov_m@bk.ru>
2021-06-11 15:26:02 +0600

    reformatted code

--- src/main/reactapp/src/App.js
+++ src/main/reactapp/src/App.js
@@ -2,36 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
 
-class App extends Component{
-  state = {
-    counterparties: []
-  };
+class App extends Component {
+    state = {
+        counterparties: []
+    };
 
-  async componentDidMount() {
-    const response = await fetch('/counterparties');
-    const body = await response.json();
-    this.setState({counterparties: body._embedded.counterparties});
-  }
+    async componentDidMount() {
+        const response = await fetch('/counterparties');
+        const body = await response.json();
+        this.setState({counterparties: body._embedded.counterparties});
+    }
 
-  render() {
-    const {counterparties} = this.state;
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <div className="App-intro">
-              <h2>Контрагенты</h2>
-              {counterparties.map(counterparty =>
-                  <div key={counterparty.id}>
-                    {counterparty.name} {counterparty.tin} {counterparty.iec}
-                    {counterparty.accountNumber} {counterparty.bic}
-                  </div>
-              )}
+    render() {
+        const {counterparties} = this.state;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <div className="App-intro">
+                        <h2>Контрагенты</h2>
+                        {counterparties.map(counterparty =>
+                            <div key={counterparty.id}>
+                                {counterparty.name} {counterparty.tin} {counterparty.iec}
+                                {counterparty.accountNumber} {counterparty.bic}
+                            </div>
+                        )}
+                    </div>
+                </header>
             </div>
-          </header>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;

68c022ee525a586d07a4269e1dc2da1c719f8c07
Marif Manafov <manafov_m@bk.ru>
2021-06-11 16:53:08 +0600

    added home component

---
+++ src/main/reactapp/src/Home.js
@@ -0,0 +1,20 @@
+import {Component} from "react";
+import AppNavbar from "./AppNavbar";
+import {Button, Container} from "reactstrap";
+import {Link} from "react-router-dom";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    <Button color="link">
+                        <Link to="/counterparties">Контрагенты</Link>
+                    </Button>
+                </Container>
+            </div>
+        );
+    }
+}
+export default Home;

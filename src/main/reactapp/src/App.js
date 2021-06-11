f1a51b7916297926a682a03611a3707fdcba07a8
Marif Manafov <manafov_m@bk.ru>
2021-06-11 14:46:06 +0600

    fetched counterparties from rest api

--- src/main/reactapp/src/App.js
+++ src/main/reactapp/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    counterparties: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/counterparties');
+    const body = await response.json();
+    this.setState({counterparties: body._embedded.counterparties});
+  }
+
+  render() {
+    const {counterparties} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <div className="App-intro">
+              <h2>Контрагенты</h2>
+              {counterparties.map(counterparty =>
+                  <div key={counterparty.id}>
+                    {counterparty.name} {counterparty.tin} {counterparty.iec}
+                    {counterparty.accountNumber} {counterparty.bic}
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;

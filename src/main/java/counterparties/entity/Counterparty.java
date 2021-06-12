9a7650b16c29b431711d96dac2001ec603a162ed
Marif Manafov <manafov_m@bk.ru>
2021-06-12 11:49:09 +0600

    added hibernate validation

--- src/main/java/counterparties/entity/Counterparty.java
+++ src/main/java/counterparties/entity/Counterparty.java
@@ -5,10 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Builder
 @AllArgsConstructor
@@ -19,9 +18,25 @@ public class Counterparty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
+    @NotNull(message = "Введите наименование контрагента.")
+    @Size(min = 3, max = 20, message = "Наименование должно быть длиной от {min} до {max} символов.")
     private String name;
+
+    @NotNull(message = "Введите ИНН контрагента.")
+    @Size(min = 10, max = 10, message = "ИНН должен быть длиной {min} символов.")
     private String tin;
+
+    @NotNull(message = "Введите КПП контрагента.")
+    @Size(min = 9, max = 9, message = "КПП должен быть длиной {min} символов.")
     private String iec;
+
+    @NotNull(message = "Введите номер счета контрагента.")
+    @Size(min = 20, max = 20, message = "Номер счета должен быть длиной {min} символов.")
     private String accountNumber;
+
+    @NotNull(message = "Введите БИК банка контрагента")
+    @Size(min = 9, max = 9, message = "БИК должен быть длиной {min} символов.")
     private String bic;
 }

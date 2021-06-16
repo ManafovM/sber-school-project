c3c383754f6d63b0b33ae134375dbd43bb7b3973
Marif Manafov <manafov_m@bk.ru>
2021-06-16 15:13:20 +0600

    added documented annotation

--- src/main/java/counterparties/service/constraint/AccountNumberAndBicConstraint.java
+++ src/main/java/counterparties/service/constraint/AccountNumberAndBicConstraint.java
@@ -4,11 +4,9 @@ import counterparties.service.validator.AccountNumberAndBicValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Documented
 @Constraint(validatedBy = AccountNumberAndBicValidator.class)
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
